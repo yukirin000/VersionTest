9cb2398113560f1b13aea2152d0103752abff7be
yukirin <yukirin_000@163.com>
2015-07-04 11:40:58 +0800

    gg

---
+++ JLXCSNS_iOS/JLXCSNS_iOS/App/JLXC/Demo/Controller/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  JLXCSNS_iOS
+//
+//  Created by 李晓航 on 15/7/3.
+//  Copyright (c) 2015年 JLXC. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface TTViewController : BaseViewController
+
+@end
--- JLXCSNS_iOS/JLXCSNS_iOS/App/JLXC/Demo/Controller/ViewController.m
+++ JLXCSNS_iOS/JLXCSNS_iOS/App/JLXC/Demo/Controller/ViewController.m
@@ -77,6 +77,7 @@
     
     NSLog(@"ggg");
     debugLog(@"hgahah");
+    
 }
 
 #pragma mark- private methods
